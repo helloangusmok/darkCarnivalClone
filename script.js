35f29c100f5fac344746015b643461692d4f1b7d
helloangusmok <34521800+helloangusmok@users.noreply.github.com>
2018-01-28 15:58:53 +1100

    Add files via upload
    Fixed background switch, added logo

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
     $(".invisible-block-right").mouseenter(function(){
-        $("body").addClass("black", 5000);
+        $("body").addClass("black");
+        $(".bg").css("opacity", 1);
+        $("#logo").css("filter", brightness(0));
     });
     $(".invisible-block-left").mouseenter(function(){
-        $("body").removeClass("black", 5000);
+        $("body").removeClass("black");
+        $(".bg").css( "opacity", 0);
     });
 });
