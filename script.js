8a4f28cc8d4be04f672857d15a5e02034e2c712a
helloangusmok <34521800+helloangusmok@users.noreply.github.com>
2018-02-02 16:46:09 +1100

    Add files via upload
    bgswitcher used for background image switching

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
+var backgroundSwitcher = $("body").bgswitcher({
+    images: ["dark-background.png", "background-light.jpg"],
+    loop: false
+    });
+
 $(document).ready(function() {
     $(".invisible-block-right").mouseenter(function(){
         $("body").addClass("black");
-        $(".bg").css("opacity", 1);
-        $("#logo").css("filter", brightness(0));
+        // $(".bg").css("opacity", 1);
+        backgroundSwitcher.bgswitcher("select", 1);
     });
     $(".invisible-block-left").mouseenter(function(){
         $("body").removeClass("black");
-        $(".bg").css( "opacity", 0);
+        // $(".bg").css( "opacity", 0);
+        backgroundSwitcher.bgswitcher("select", 0);
     });
 });
